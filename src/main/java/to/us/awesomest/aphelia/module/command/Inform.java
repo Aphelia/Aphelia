02fa25e2388fdf8a05c5c8d444154c179c09a0f3
Raymond Xu <raymond.j.xu@gmail.com>
2020-06-19 11:30:16 -0400

    Fix bugs

--- src/main/java/to/us/awesomest/aphelia/module/command/Inform.java
+++ src/main/java/to/us/awesomest/aphelia/module/command/Inform.java
@@ -8,10 +8,10 @@ import net.dv8tion.jda.api.entities.User;
 import to.us.awesomest.aphelia.module.MessagingUtils;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-@SuppressWarnings("ResultOfMethodCallIgnored")
 public class Inform implements Command {
     @Override
     public boolean isDMUsable() {
@@ -21,9 +21,9 @@ public class Inform implements Command {
     @SuppressWarnings("ConstantConditions")
     @Override
     public void run(User author, MessageChannel channel, String args, Guild guild) {
-        List<String> argsArray = Arrays.asList(args.split(" "));
+        List<String> argsArray = new ArrayList<>(Arrays.asList(args.split(" ")));
         if (argsArray.size() < 2) {
-            channel.sendMessage("Invalid syntax! Usage: !inform <title> <description> <line 1> <line 2> ... <line *n*>");
+            channel.sendMessage("Invalid syntax! Usage: !inform <title> <description> <line 1> <line 2> ... <line *n*>").queue();
             return;
         }
         if (guild != null) {
@@ -34,14 +34,14 @@ public class Inform implements Command {
         }
         EmbedBuilder informBuilder = new EmbedBuilder();
         informBuilder.setColor(new Color(0, 22, 88));
-        informBuilder.setTitle(argsArray.remove(1));
-        informBuilder.setDescription(argsArray.remove(1));
+        informBuilder.setTitle(argsArray.remove(0));
+        informBuilder.setDescription(argsArray.remove(0));
         for (String arg : argsArray) {
             informBuilder.addField(" ", arg, false);
         }
-        informBuilder.addBlankField(false);
-        informBuilder.addField(" ", "*Like this? [Invite me!](aphelia.github.io/invite)*", false);
+        informBuilder.addField(" ", "*Like this? [Invite me](https://aphelia.github.io/invite)!*", false);
         informBuilder.setFooter("Sent by " + author.getName(), author.getAvatarUrl());
+        channel.sendMessage(informBuilder.build()).queue();
     }
 
     @Override
