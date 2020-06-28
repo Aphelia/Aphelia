8489b3af6a46a51405613eb3ee082c61c5d59f31
Raymond Xu <raymond.j.xu@gmail.com>
2020-06-28 15:45:24 -0400

    fix parsing bug (tempfix)
    Signed-off-by: Raymond Xu <raymond.j.xu@gmail.com>

--- src/main/java/to/us/awesomest/aphelia/module/command/ConnectMC.java
+++ src/main/java/to/us/awesomest/aphelia/module/command/ConnectMC.java
@@ -25,7 +25,7 @@ public class ConnectMC implements Command {
             MessagingUtils.sendNoPermissions(channel, "Manage Server");
             return;
         }
-        if (args.trim().isEmpty()) {
+        if (args.trim().toLowerCase().contains("connectmc")) {
             EmbedBuilder commandListBuilder = new EmbedBuilder();
             commandListBuilder
                     .setColor(new Color(255, 0, 0))
