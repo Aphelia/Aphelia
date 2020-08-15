ac1bc6ea114701d5baff3284f9975500a99f0801
Raymond Xu <raymond.j.xu@gmail.com>
2020-08-15 09:53:40 -0400

    Add Aphelia AdBypass role and some simplification (thanks IntelliJ, I'm bad at regex)
    Signed-off-by: Raymond Xu <raymond.j.xu@gmail.com>

--- src/main/java/to/us/awesomest/aphelia/module/chathandlers/URLFilterHandler.java
+++ src/main/java/to/us/awesomest/aphelia/module/chathandlers/URLFilterHandler.java
@@ -24,7 +24,12 @@ public class URLFilterHandler implements ChatHandler {
 
     @Override
     public void run(Message message) {
-        if(message.getContentRaw().matches(".*[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*).*")) {
+        @SuppressWarnings("ConstantConditions")
+        boolean hasAdBypass = (message.getMember().getRoles().stream()
+                .filter(role -> role.getName().equals("Aphelia AdBypass")) // filter by role name
+                .count()) >= 1; //probably a cleaner way to do this but I don't
+        if(hasAdBypass) return;
+        if(message.getContentRaw().matches(".*[-a-zA-Z0-9@:%._+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_+.~#?&/=]*).*")) {
             MessageChannel channel = message.getChannel();
             message.delete().complete(); //I do want it to block.
             EmbedBuilder feedbackBuilder = new EmbedBuilder();
