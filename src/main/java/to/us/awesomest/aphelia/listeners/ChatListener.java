90790d5f6df31fff35ca7c273a85b9506e0078ba
Raymond Xu <raymond.j.xu@gmail.com>
2020-08-22 22:09:38 -0400

    make listener handling async

--- src/main/java/to/us/awesomest/aphelia/listeners/ChatListener.java
+++ src/main/java/to/us/awesomest/aphelia/listeners/ChatListener.java
@@ -11,18 +11,30 @@ import to.us.awesomest.aphelia.module.chathandlers.URLFilterHandler;
 
 
 public class ChatListener extends ListenerAdapter {
+    long processorThreadCount = 0;
     @SubscribeEvent
     public void onMessageReceivedEvent(MessageReceivedEvent event) {
-        if(event.getAuthor().isBot()) return;
+        new Thread(new Processor(event), "ChatListener Processor " + ++processorThreadCount).start();
+    }
 
-        LoggerFactory.getLogger("ChatListener").debug("Processing message " + event.getMessage().getContentRaw());
-        if(event.isFromGuild()) {
-            if(ModuleManager.getInstanceByGuildId(event.getGuild().getId()).runCommands(event.getMessage())) return;
-            if(URLFilterHandler.getURLFilterHandlerByGuildId(event.getGuild().getId()).run(event.getMessage())) return;
-            LoggerFactory.getLogger("ChatListener").debug("Did not find relevant command for message " + event.getMessage().getContentRaw() + ", going to shortcuts.");
-            ShortcutHandler.getShortcutHandlerByGuildId(event.getGuild().getId()).run(event.getMessage());
-            LoggerFactory.getLogger("ChatListener").debug("Sending to MC " + event.getMessage().getContentRaw());
-        } else if(ModuleManager.getDMInstance().runCommands(event.getMessage())) return;
-        MCChannelHandler.getInstance().run(event.getMessage());
+    private static class Processor implements Runnable {
+        MessageReceivedEvent event;
+        private Processor(MessageReceivedEvent event) {
+            this.event = event;
+        }
+        @Override
+        public void run() {
+            if(event.getAuthor().isBot()) return;
+
+            LoggerFactory.getLogger("ChatListener Processor").debug("Processing message " + event.getMessage().getContentRaw());
+            if(event.isFromGuild()) {
+                if(URLFilterHandler.getURLFilterHandlerByGuildId(event.getGuild().getId()).run(event.getMessage())) return;
+                if(ModuleManager.getInstanceByGuildId(event.getGuild().getId()).runCommands(event.getMessage())) return;
+                LoggerFactory.getLogger("ChatListener Processor").debug("Did not find relevant command for message " + event.getMessage().getContentRaw() + ", going to shortcuts.");
+                ShortcutHandler.getShortcutHandlerByGuildId(event.getGuild().getId()).run(event.getMessage());
+            } else if(ModuleManager.getDMInstance().runCommands(event.getMessage())) return;
+            LoggerFactory.getLogger("ChatListener Processor").debug("Sending to MC " + event.getMessage().getContentRaw());
+            MCChannelHandler.getInstance().run(event.getMessage());
+        }
     }
 }
